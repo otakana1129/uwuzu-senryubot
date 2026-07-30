2b9db4c9e1505b3159608bdf9ad11d67da73cee0
otakana1129 <oyagi.t308@gmail.com>
2026-07-30 11:26:17 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,20 @@
 import os
 from uwuzu import Uwuzu
 
-print("BOT START")
-
 DOMAIN = os.environ["UWUZU_DOMAIN"]
 TOKEN = os.environ["UWUZU_TOKEN"]
 
 client = Uwuzu(DOMAIN, TOKEN)
 
+print("BOT START")
+
 try:
-    timeline = client.get_timeline(limit=5)
-
-    print("件数:", len(timeline))
-
-    for post in timeline:
-        print("投稿発見")
-        try:
-            print(post.text_content)
-        except Exception as e:
-            print("投稿表示エラー:", e)
+    me = client.get_me()
+    print("ログイン成功")
+    print(me)
 
 except Exception as e:
-    print("エラー発生:", e)
+    print("ログイン失敗")
+    print(e)
 
 print("BOT END")
